a9922d2f65cb9a504b344f044cef1ca49bd89f11
bradreeder <bradjohnreeder@gmail.com>
2016-08-07 20:05:11 +0100

    set up react.js

---
+++ src/root.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Root extends React.Component {
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
+
+ReactDOM.render(<Root />, document.getElementById('container'));
---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  entry: './src/root.jsx',
+  output: {
+    path: './public/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.js(x)*$/,
+      loaders: ['babel'],
+      exclude: /node_modules/,
+    }],
+  },
+  devtool: 'source-map',
+};
