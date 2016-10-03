154750ec2813fc67b28c24d3875d3199dd0527da
bradreeder <bradjohnreeder@gmail.com>
2016-10-03 23:39:44 +0100

    updated portfolio section

--- src/components/inspiration.jsx
+++ src/components/inspiration.jsx
@@ -3,13 +3,11 @@ import React, { Component } from 'react';
 
 class Inspiration extends Component {
   render() {
-    const quote = '"but screw your courage to the sticking place and we’ll not fail" ' +
-    '(Macbeth Act I Scene VII)';
+    const quote = 'New site coming soon...';
     return (
       <div>
         <div id="inspiration"><p>{quote}</p></div>
-        <div id="inspiration-mobile"><p>'"but screw your courage to the sticking
-        place and we'll not fail"'</p></div>
+        <div id="inspiration-mobile"><p>{quote}</p></div>
       </div>
     );
   }
--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -18,9 +18,10 @@ class Portfolio extends Component {
           <p>For a full list of my projects please see my <a href="https://github.com/bradreeder?tab=repositories">
           github page</a>.</p>
           <p className="year-text">2016 (3)</p>
-          <p><a href="https://github.com/bradreeder/Tile-game">Tile-Game</a> TBC</p>
-          <p><a href="https://github.com/saferTogether">SaferTogether</a> TBC</p>
-          <p><a href="https://github.com/SafeLives">SafeLives</a> TBC</p>
+          <p><a href="https://github.com/bradreeder/Tile-game">Tile-Game</a>A tile floor puzzle-game developed as a progressive web application. In progress.</p>
+          <p><a href="https://github.com/CYPIAPT-LNDSE/breathing-with-kitty">Anna Freud Centre</a>I worked in a small team building a progressive web application aimed at young people with generalised anxiety disorders. The app improves resilience from stress by guiding the user through a variety of breathing and relaxation techniques, demonstrated using SVG and Greensock.js animations.</p>
+          <p><a href="https://github.com/saferTogether/saferTogether">SaferTogether</a>I helped build a design prototype of a risk assessment survey targeted at victims of domestic abuse. As a project manager, my additional responsibilities included managing communications with the product owners, guiding them through user testing, and ensuring agile practices were followed.</p>
+          <p><a href="https://github.com/worthwhile-charity/hack-day">Worthwhile</a>I helped build a design prototype for Worthwhile that automates the process of generating and issuing contracts and invoices for clients who find a job through the charity, using handlebars.js for templating.</p>
         </div>
         <h2>technologies</h2>
         {technologies}
